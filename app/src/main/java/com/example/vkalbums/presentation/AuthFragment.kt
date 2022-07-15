7ddf12858626da60bec18cc2b8d187b3b7aaab07
seregious <seregious@gmail.com>
2022-07-15 20:34:06 +0300

    modify auth fragment

--- app/src/main/java/com/example/vkalbums/presentation/AuthFragment.kt
+++ app/src/main/java/com/example/vkalbums/presentation/AuthFragment.kt
@@ -1,18 +1,26 @@
 package com.example.vkalbums.presentation
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
 import android.webkit.WebViewClient
-import com.example.vkalbums.R
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.example.vkalbums.data.Constants
 import com.example.vkalbums.databinding.FragmentAuthBinding
 
 
 class AuthFragment : Fragment() {
 
     private lateinit var binding: FragmentAuthBinding
+    private val viewModel: ViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,11 +30,53 @@ class AuthFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupWebView()
+        checkUrl()
+    }
+
     private fun setupWebView() = with(binding) {
         webView.webViewClient = WebViewClient()
-        webView.loadUrl("https://www.google.com/?client=safari")
+        webView.loadUrl(Constants.AUTH_URL)
         webView.settings.javaScriptEnabled = true
         webView.settings.setSupportZoom(true)
+    }
+
+    private fun checkUrl() = with(binding){
+        webView.webViewClient = object : WebViewClient() {
+            @Deprecated("Deprecated in Java")
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                if (url != null) {
+                    webView.loadUrl(url)
+                    if (url.length == 277) {
+                        Toast.makeText(activity,"ACCEPT", Toast.LENGTH_SHORT).show()
+                        Log.d("url", "$url")
+                    } else {
+                        Toast.makeText(activity,"$url", Toast.LENGTH_SHORT).show()
+                        Log.d("url", "$url")
+                    }
+                }
+                return true
+            }
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        val callback: OnBackPressedCallback =
+            object : OnBackPressedCallback(true)
+            {
+                override fun handleOnBackPressed() {
+                    if (binding.webView.canGoBack()) {
+                        binding.webView.goBack()
+                    }
+                }
+            }
+        requireActivity().onBackPressedDispatcher.addCallback(
+            this,
+            callback
+        )
     }
 
 }
