a1bd5a25490b9c25f1f211544594e42600b0781f
seregious <seregious@gmail.com>
2022-07-18 00:15:38 +0300

    update view model with new network requests

--- app/src/main/java/com/example/vkalbums/presentation/ViewModel.kt
+++ app/src/main/java/com/example/vkalbums/presentation/ViewModel.kt
@@ -4,7 +4,6 @@ import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.vkalbums.data.DownloadSingleton
 import com.example.vkalbums.domain.Album
 import com.example.vkalbums.data.AlbumsRequest
 import com.example.vkalbums.data.PhotosRequest
@@ -23,53 +22,43 @@ class ViewModel: ViewModel() {
         MutableLiveData<Album>()
     }
 
+    val currentPhoto: MutableLiveData<Photo> by lazy {
+        MutableLiveData<Photo>()
+    }
+
     var albumsList = MutableLiveData<List<Album>>()
     var photosList = MutableLiveData<List<Photo>>()
 
     fun getAlbums() {
         var list: List<Album>
-        val request = makeAlbumsRequest()
-        viewModelScope.launch {
-            try {
-                list = DownloadSingleton.getAlbums.getAlbumsByToken(request).response.items
-                albumsList.postValue(list)
-            } catch(e: Exception) {
-                Log.d("url", "fail download albums")
+        if (currentUser.value != null) {
+            val userId = currentUser.value!!.id
+            val userToken = currentUser.value!!.token
+            viewModelScope.launch {
+                try {
+                    list = AlbumsRequest.get(userId,userToken)
+                    albumsList.postValue(list)
+                } catch (e: Exception) {
+                    Log.d("url", "fail download albums")
+                }
             }
         }
     }
 
     fun getPhotos() {
         var list: List<Photo>
-        val request = makePhotoRequest()
-        viewModelScope.launch {
-            try {
-                list = DownloadSingleton.getPhotos.getPhotos(request).response.items
-                photosList.postValue(list)
-            } catch(e: Exception) {
-                Log.d("url", "fail download photos")
-            }
-        }
-    }
-
-    private fun makeAlbumsRequest(): String {
-        var request = ""
-        if (currentUser.value != null) {
-            val userId = currentUser.value!!.id
-            val userToken = currentUser.value!!.token
-            request = AlbumsRequest.makeRequest(userId, userToken)
-        }
-        return request
-    }
-
-    private fun makePhotoRequest(): String {
-        var request = ""
         if (currentUser.value != null) {
             val userId = currentUser.value!!.id
             val userToken = currentUser.value!!.token
             val album = currentAlbum.value!!.id.toString()
-            request = PhotosRequest.makeRequest(userId,userToken,album)
+            viewModelScope.launch {
+                try {
+                    list = PhotosRequest.get(userId, album, userToken)
+                    photosList.postValue(list)
+                } catch (e: Exception) {
+                    Log.d("url", "fail download photos")
+                }
+            }
         }
-        return request
     }
 }
