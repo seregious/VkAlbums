c4035cca92f3d904af05ab5562c06f88e03a26a0
seregious <seregious@gmail.com>
2022-07-17 14:08:24 +0300

    testing albums interface

--- app/src/main/java/com/example/vkalbums/data/AlbumsDownload.kt
+++ app/src/main/java/com/example/vkalbums/data/AlbumsDownload.kt
@@ -5,9 +5,9 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object AlbumsDownload {
     var retrofit: Retrofit = Retrofit.Builder()
-        .baseUrl(Constants.BASE_URL)
+        .baseUrl("https://api.vk.com/method/")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
-    var getPhotos = retrofit.create(AlbumsInterface::class.java)
+    var getAlbums = retrofit.create(AlbumsInterface::class.java)
 }
