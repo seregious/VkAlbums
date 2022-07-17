309a5c9d7b02b5827cf9b6030597afb685e3e222
seregious <seregious@gmail.com>
2022-07-18 00:19:01 +0300

    setup singleton for retrofit

--- app/src/main/java/com/example/vkalbums/data/network/DownloadSingleton.kt
+++ app/src/main/java/com/example/vkalbums/data/network/DownloadSingleton.kt
@@ -1,14 +1,15 @@
-package com.example.vkalbums.data
+package com.example.vkalbums.data.network
 
+import com.example.vkalbums.data.Constants
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object DownloadSingleton {
-    var retrofit: Retrofit = Retrofit.Builder()
+    private var retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(Constants.BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
     var getAlbums: AlbumsInterface = retrofit.create(AlbumsInterface::class.java)
-    var getPhotos = retrofit.create(PhotosInterface::class.java)
+    var getPhotos: PhotosInterface = retrofit.create(PhotosInterface::class.java)
 }
