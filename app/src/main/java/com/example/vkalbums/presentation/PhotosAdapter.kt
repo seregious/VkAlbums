10dd49d5ad959af60aaf1e1fc5b8624865355d50
seregious <seregious@gmail.com>
2022-07-18 00:16:29 +0300

    setup photo adapter

--- app/src/main/java/com/example/vkalbums/presentation/PhotosAdapter.kt
+++ app/src/main/java/com/example/vkalbums/presentation/PhotosAdapter.kt
@@ -5,9 +5,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.vkalbums.R
-import com.example.vkalbums.databinding.AlbumCardBinding
+import com.example.vkalbums.databinding.PhotoCardBinding
 import com.example.vkalbums.domain.Photo
-import com.example.vkalbums.presentation.fragments.PhotosFragment
 import com.squareup.picasso.Picasso
 
 class PhotosAdapter(
@@ -21,11 +20,10 @@ class PhotosAdapter(
         }
 
     class PhotoHolder(view: View): RecyclerView.ViewHolder(view) {
-        private var binding = AlbumCardBinding.bind(view)
+        private var binding = PhotoCardBinding.bind(view)
 
         fun bind(photo: Photo, listener: Listener) = with(binding){
-            Picasso.get().load(photo.sizes.last().url).into(albumCover)
-            albumName.text = photo.date.toString()
+            Picasso.get().load(photo.sizes.last().url).into(photoView)
             itemView.setOnClickListener {
                 listener.onClick(photo)
             }
@@ -33,7 +31,7 @@ class PhotosAdapter(
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.album_card, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.photo_card, parent, false)
         return PhotoHolder(view)
     }
 
