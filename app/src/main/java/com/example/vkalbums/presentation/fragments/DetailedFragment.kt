36ae22e2c27c7d36fd9165f67b2b0014b5e74956
seregious <seregious@gmail.com>
2022-07-18 00:17:45 +0300

    setup fragment for showing single photo

---
+++ app/src/main/java/com/example/vkalbums/presentation/fragments/DetailedFragment.kt
@@ -0,0 +1,57 @@
+package com.example.vkalbums.presentation.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.activityViewModels
+import com.example.vkalbums.databinding.FragmentDetailedBinding
+import com.example.vkalbums.presentation.ViewModel
+import com.squareup.picasso.Picasso
+import java.time.Instant
+import java.time.ZoneId
+
+
+class DetailedFragment : Fragment() {
+
+    private lateinit var binding: FragmentDetailedBinding
+    private val viewModel: ViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentDetailedBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setTitle()
+        setImage()
+    }
+
+    private fun setTitle() {
+        val text = getDateTime()
+        (activity as AppCompatActivity).supportActionBar?.title = text
+    }
+
+    private fun setImage() {
+        val image = viewModel.currentPhoto.value?.sizes?.last()?.url
+        Picasso.get().load(image).into(binding.imageView)
+    }
+
+    private fun getDateTime(): String {
+        val date = viewModel.currentPhoto.value?.date
+        var dt = ""
+        if (date != null) {
+            dt = Instant.ofEpochSecond(date)
+                .atZone(ZoneId.systemDefault())
+                .toLocalDateTime().toString()
+        }
+        return dt
+    }
+
+}
