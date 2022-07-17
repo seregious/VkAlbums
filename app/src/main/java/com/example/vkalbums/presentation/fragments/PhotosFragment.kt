982eb5dd0fe887f749964cf35a4c5df0b4a2432f
seregious <seregious@gmail.com>
2022-07-17 15:58:37 +0300

    setup photos fragment

---
+++ app/src/main/java/com/example/vkalbums/presentation/fragments/PhotosFragment.kt
@@ -0,0 +1,46 @@
+package com.example.vkalbums.presentation.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.vkalbums.databinding.FragmentPhotosBinding
+import com.example.vkalbums.presentation.PhotosAdapter
+import com.example.vkalbums.presentation.ViewModel
+
+class PhotosFragment : Fragment() {
+
+    private lateinit var binding: FragmentPhotosBinding
+    private val viewModel: ViewModel by activityViewModels()
+    private lateinit var photosAdapter: PhotosAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentPhotosBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupPhotos()
+    }
+
+    private fun setupPhotos() {
+        viewModel.getAlbums()
+        binding.photosRecycler.layoutManager = GridLayoutManager(context, 2)
+        photosAdapter = PhotosAdapter(this)
+        binding.photosRecycler.adapter = photosAdapter
+        viewModel.photosList.observe(viewLifecycleOwner) {
+            photosAdapter.photosList = it
+        }
+    }
+
+
+
+
+}
