8d18f5c259bf8e70b1895dbb602d72d608810fee
seregious <seregious@gmail.com>
2022-07-17 14:06:20 +0300

    update albums fragment

--- app/src/main/java/com/example/vkalbums/presentation/AlbumsFragment.kt
+++ app/src/main/java/com/example/vkalbums/presentation/AlbumsFragment.kt
@@ -1,15 +1,21 @@
 package com.example.vkalbums.presentation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.vkalbums.databinding.FragmentAlbumsBinding
+import com.example.vkalbums.domain.Album
 
-class AlbumsFragment : Fragment() {
+class AlbumsFragment : Fragment(), AlbumsAdapter.Listener {
 
-    lateinit var binding: FragmentAlbumsBinding
+    private lateinit var binding: FragmentAlbumsBinding
+    private val viewModel: ViewModel by activityViewModels()
+    private lateinit var albumsAdapter: AlbumsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,6 +23,25 @@ class AlbumsFragment : Fragment() {
     ): View {
         binding = FragmentAlbumsBinding.inflate(inflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupAlbums()
+    }
+
+    private fun setupAlbums() {
+        viewModel.getAlbums()
+        binding.albumsRecycler.layoutManager = GridLayoutManager(context, 2)
+        albumsAdapter = AlbumsAdapter(this)
+        binding.albumsRecycler.adapter = albumsAdapter
+        viewModel.albumsList.observe(viewLifecycleOwner) {
+            albumsAdapter.albumsList = it
+        }
+    }
+
+    override fun onClick(album: Album) {
+
     }
 
 }
