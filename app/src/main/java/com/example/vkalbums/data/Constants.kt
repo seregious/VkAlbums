e779ff901eb37157606abac1ab87feff21550568
seregious <seregious@gmail.com>
2022-07-18 00:18:06 +0300

    update constants, delete useless

--- app/src/main/java/com/example/vkalbums/data/Constants.kt
+++ app/src/main/java/com/example/vkalbums/data/Constants.kt
@@ -2,6 +2,5 @@ package com.example.vkalbums.data
 
 object Constants {
     const val AUTH_URL = "https://oauth.vk.com/authorize?client_id=8215234&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=photos&response_type=token&v=5.131"
-    const val BASE_URL = "https://api.vk.com/"
-    const val API: String = "&v=5.131"
+    const val BASE_URL = "https://api.vk.com/method/"
 }
