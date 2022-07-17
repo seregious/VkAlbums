05a8ad9ff364a17a93c55a0f068dcfe561011434
seregious <seregious@gmail.com>
2022-07-17 15:56:40 +0300

    update auth fragment

--- app/src/main/java/com/example/vkalbums/presentation/fragments/AuthFragment.kt
+++ app/src/main/java/com/example/vkalbums/presentation/fragments/AuthFragment.kt
@@ -1,4 +1,4 @@
-package com.example.vkalbums.presentation
+package com.example.vkalbums.presentation.fragments
 
 import android.content.Context
 import android.os.Bundle
@@ -14,6 +14,7 @@ import com.example.vkalbums.R
 import com.example.vkalbums.data.Constants
 import com.example.vkalbums.databinding.FragmentAuthBinding
 import com.example.vkalbums.domain.User
+import com.example.vkalbums.presentation.ViewModel
 
 
 class AuthFragment : Fragment() {
