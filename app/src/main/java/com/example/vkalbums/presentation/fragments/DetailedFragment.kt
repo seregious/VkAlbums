9a8e704a911510e8562664f2af7dcc5ea8a8ae57
seregious <seregious@gmail.com>
2022-07-18 20:58:53 +0300

    add share button

--- app/src/main/java/com/example/vkalbums/presentation/fragments/DetailedFragment.kt
+++ app/src/main/java/com/example/vkalbums/presentation/fragments/DetailedFragment.kt
@@ -1,11 +1,13 @@
 package com.example.vkalbums.presentation.fragments
 
+
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.StrictMode
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.example.vkalbums.databinding.FragmentDetailedBinding
 import com.example.vkalbums.presentation.ViewModel
@@ -53,5 +55,4 @@ class DetailedFragment : Fragment() {
         }
         return dt
     }
-
 }
