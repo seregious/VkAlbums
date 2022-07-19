db0fe7f5d1d39f07f6332b84cc813215c4593fe2
seregious <seregious@gmail.com>
2022-07-19 17:19:41 +0300

    add cancel url constant

--- app/src/main/java/com/example/vkalbums/data/Constants.kt
+++ app/src/main/java/com/example/vkalbums/data/Constants.kt
@@ -3,4 +3,6 @@ package com.example.vkalbums.data
 object Constants {
     const val AUTH_URL = "https://oauth.vk.com/authorize?client_id=8215234&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=photos&response_type=token&v=5.131"
     const val BASE_URL = "https://api.vk.com/method/"
+    const val TOKEN_URL = "https://oauth.vk.com/blank.html#access_token=vk1"
+    const val DENIED_URL = "User%20denied%20your%20request"
 }
